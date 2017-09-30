88b4571e24e5b49abc35673a148d566b5970ffab
Anderson-Karena <30563088+Cakarena@users.noreply.github.com>
2017-09-30 13:26:35 +1000

    Add files via upload

---
+++ first_two.py
@@ -0,0 +1,17 @@
+##Given a string, return the string made of its first two chars, 
+##so the String "Hello" yields "He". If the string is shorter than length 2, 
+##return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
+## https://consoleart.wordpress.com/2010/11/25/coding-bat-solutions-solution/
+
+def first_two(str):
+	print (str[0:2])
+
+
+first_two('Hello')
+first_two('abcdefg')
+first_two('ab')
+first_two('a ') ##only way I can make this appear is putting in a space
+first_two('  ') ## have to create two spaces to be recognised
+first_two('kitten')
+first_two('hi')
+first_two('hiya')
