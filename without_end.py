636123717c59c58089bc3b14d3ccbfc431f5b0fc
Anderson-Karena <30563088+Cakarena@users.noreply.github.com>
2017-09-30 13:58:01 +1000

    Add files via upload

---
+++ without_end.py
@@ -0,0 +1,14 @@
+##Given a string, return a version without the first and last char,
+## so "Hello" yields "ell". The string length will be at least 2.
+def without_end(str):
+	print (str[1:-1])
+
+without_end("hello")
+without_end("java")
+without_end("coding")
+without_end("code")
+without_end("ab")
+without_end("chocolate")
+without_end("kitten")
+without_end("woohoo")
+
