58bb5eb8c24525f5c3ef308172f7c111d8e72a83
Anderson-Karena <30563088+Cakarena@users.noreply.github.com>
2017-09-30 13:28:05 +1000

    Update first_two.py

--- first_two.py
+++ first_two.py
@@ -1,7 +1,7 @@
 ##Given a string, return the string made of its first two chars, 
 ##so the String "Hello" yields "He". If the string is shorter than length 2, 
 ##return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
-## https://consoleart.wordpress.com/2010/11/25/coding-bat-solutions-solution/
+
 
 def first_two(str):
 	print (str[0:2])
